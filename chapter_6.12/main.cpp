6a9aba2222bcdd9edebcf5f0e44c9d7f96c12601
Przemyslaw Krolak <przemas@kompajn>
2019-03-10 22:21:10 +0100

    Chapter 6.12 - raw pointer replaced with std::unique_ptr

--- chapter_6.12/main.cpp
+++ chapter_6.12/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
+#include <memory>
 
 template <typename T>
 class Table final
 {
   private:
-    T* tbl { nullptr };
+    std::unique_ptr<T[]> tbl { nullptr };
     const size_t TBL_SIZE {};
 
   public:
     Table() = delete;
-    Table(const Table&) = delete;
+    Table(const Table<T>&);
+    Table(Table<T>&&);
     Table(std::initializer_list<T> li);
-    ~Table();
+    ~Table() = default;
 
     T& operator[](int index) const { return tbl[index]; }
-    T* begin() const { return tbl; }
-    T* end() const { return tbl + TBL_SIZE; }
-    const T* cbegin() const { return tbl; }
-    const T* cend() const { return tbl + TBL_SIZE; }
+    T* begin() const { return tbl.get(); }
+    T* end() const { return tbl.get() + TBL_SIZE; }
+    const T* cbegin() const { return tbl.get(); }
+    const T* cend() const { return tbl.get() + TBL_SIZE; }
+    size_t size() const { return TBL_SIZE; };
+
 };
 
 template <typename T>
@@ -25,6 +29,7 @@ Table<T>::Table(std::initializer_list<T> li)
   : tbl { new T[ li.size() ] },
     TBL_SIZE { li.size() }
 {
+  std::cout << "Custom ctor" << std::endl;
   int index {};
   for (const auto& element : li)
   {
@@ -33,15 +38,37 @@ Table<T>::Table(std::initializer_list<T> li)
 }
 
 template <typename T>
-Table<T>::~Table()
+Table<T>::Table(const Table<T>& table)
+  : tbl { new T[ table.size() ] },
+    TBL_SIZE { table.size() }
 {
-  delete [] tbl;
+  std::cout << "Copy ctor" << std::endl;
+  int index {};
+  for (const auto& element : table)
+  {
+    tbl[index++] = element;
+  }
 }
+
+template <typename T>
+Table<T>::Table(Table<T>&& table)
+  : tbl { std::move(table.tbl) },
+    TBL_SIZE { table.size() }
+{
+  std::cout << "Move ctor" << std::endl;
+}
+
+template <typename T, typename... Args>
+Table<T> createTable(T&& arg0, Args&&... args)
+{
+  return Table<T> {std::forward<T>(arg0), std::forward<T>(args)...};
+}
+
 
 int main()
 {
 
-  Table tbl {1.34, 2.5, 3.23, 4.23, 5.0};
+  Table<float> tbl {1.34, 2.5, 3.23, 4.23, 5.0};
 
   tbl[2] = 4353;
 
@@ -49,6 +76,27 @@ int main()
   {
     std::cout << element << std::endl;
   }
+
+  std::cout << "----------------" << std::endl;
+
+  const Table<float> copyTable {tbl};
+
+  for(auto element : copyTable)
+  {
+    std::cout << element << std::endl;
+  }
+
+  std::cout << "----------------" << std::endl;
+
+  const Table<float> moveTable { createTable(1.234f, 2323.25f, 435.234f) };
+
+  moveTable[4] = 7.77;
+
+  for(auto element : moveTable)
+  {
+    std::cout << element << std::endl;
+  }
+
 
   return 0;
 }
