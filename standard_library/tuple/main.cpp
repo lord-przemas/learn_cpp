2c50118f88ceb1a2ba97764ac73a4fcc72f6c9c6
Przemyslaw Krolak <przemas@kompajn>
2019-05-30 00:31:57 +0200

    Standard Library: Tuple + get function (Version 2)

--- standard_library/tuple/main.cpp
+++ standard_library/tuple/main.cpp
@@ -13,7 +13,7 @@ class Qpa
     }
 };
 
-namespace my
+namespace mystd
 {
 
   template<typename T, typename... Types>
@@ -68,66 +68,116 @@ namespace my
       T get() const { return element; }
   };
 
-  template<bool, typename T, typename... Types>
-  class tuple_element_t
+  namespace v1
   {
-    public:
-      constexpr tuple_element_t(const tuple<T, Types...>& tup)
+    template<bool, typename T, typename... Types>
+    class tuple_element_t
+    {
+      public:
+        constexpr tuple_element_t(const tuple<T, Types...>& tup)
+        {
+        }
+    };
+
+    template<typename T, typename... Types>
+    class tuple_element_t<true, T, Types...>
+    {
+      private:
+        T element;
+
+      public:
+        using type = T;
+
+        constexpr tuple_element_t(const tuple<T, Types...>& tup)
+        : element {tup.get()}
+        {
+        }
+
+        T get() const { return element; }
+    };
+
+    template<std::size_t A, std::size_t X,  typename T, typename... Types>
+    class tuple_element_find : public tuple_element_find<A+1, X, Types...>, public tuple_element_t<A == X, T, Types...>
+    {
+      public:
+        constexpr tuple_element_find(const tuple<T, Types...>& tup)
+        : tuple_element_find<A+1, X, Types...> {tup},
+          tuple_element_t<A == X, T, Types...> {tup}
+        {
+        }
+    };
+
+    template<std::size_t A, std::size_t X, typename T>
+    class tuple_element_find<A,X,T> : public tuple_element_t<A == X, T>
+    {};
+
+    template<std::size_t I, typename = void>
+    class tuple_element;
+
+    template<std::size_t I, typename... Types>
+    class tuple_element<I, tuple<Types...>> : public tuple_element_find<0, I, Types...>
+    {
+      public:
+        constexpr tuple_element(const tuple<Types...>& tup)
+        : tuple_element_find<0, I, Types...> {tup}
+        {
+        }
+    };
+
+    template< std::size_t I, class... Types >
+    constexpr typename tuple_element<I, tuple<Types...> >::type
+    get( const tuple<Types...>& t )
+    {
+      tuple_element<I, tuple<Types...>> element {t};
+      return element.get();
+    }
+
+  }
+
+  namespace v2
+  {
+
+    template <int Start, int End, typename T, typename... Types>
+    struct tuple_element_impl;
+
+    template <int Start, int End, typename T, typename... Types>
+    struct tuple_element_impl<Start, End, tuple<T, Types...>>
+    {
+      using element_type = typename tuple_element_impl<Start+1, End, tuple<Types...>>::element_type;
+      using tuple_type = typename tuple_element_impl<Start+1, End, tuple<Types...>>::tuple_type;
+    };
+
+    template <int End, typename T, typename... Types>
+    struct tuple_element_impl<End, End, tuple<T, Types...>>
+    {
+      using element_type = T;
+      using tuple_type = tuple<T, Types...>;
+    };
+
+    template <int I, typename... Types>
+    struct tuple_element;
+
+    template <int I, typename... Types>
+    struct tuple_element<I, tuple<Types...>>
+    {
+      using element_type = typename tuple_element_impl<0, I, tuple<Types...>>::element_type;
+      using tuple_type = typename tuple_element_impl<0, I, tuple<Types...>>::tuple_type;
+      const element_type value {};
+
+      tuple_element(const tuple<Types...>& t)
+        : value {static_cast<const tuple_type&>(t).get()}
       {
       }
-  };
+    };
 
-  template<typename T, typename... Types>
-  class tuple_element_t<true, T, Types...>
-  {
-    private:
-      T element;
+    template< std::size_t I, class... Types >
+    constexpr typename tuple_element<I, tuple<Types...> >::element_type
+    get( const tuple<Types...>& t )
+    {
+      tuple_element<I, tuple<Types...>> element {t};
+      return element.value;
+    }
 
-    public:
-      using type = T;
-
-      constexpr tuple_element_t(const tuple<T, Types...>& tup)
-      : element {tup.get()}
-      {
-      }
-
-      T get() const { return element; }
-  };
-
-  template<std::size_t A, std::size_t X,  typename T, typename... Types>
-  class tuple_element_find : public tuple_element_find<A+1, X, Types...>, public tuple_element_t<A == X, T, Types...>
-  {
-    public:
-      constexpr tuple_element_find(const tuple<T, Types...>& tup)
-      : tuple_element_find<A+1, X, Types...> {tup}, 
-        tuple_element_t<A == X, T, Types...> {tup}
-      {
-      }
-  };
-
-  template<std::size_t A, std::size_t X, typename T>
-  class tuple_element_find<A,X,T> : public tuple_element_t<A == X, T>
-  {};
-  
-  template<std::size_t I, typename = void>
-  class tuple_element;
-
-  template<std::size_t I, typename... Types>
-  class tuple_element<I, tuple<Types...>> : public tuple_element_find<0, I, Types...> 
-  {
-    public:
-      constexpr tuple_element(const tuple<Types...>& tup)
-      : tuple_element_find<0, I, Types...> {tup}
-      {
-      }
-  };
-
-  template< std::size_t I, class... Types >
-  constexpr typename tuple_element<I, tuple<Types...> >::type
-  get( const tuple<Types...>& t )
-  {
-    tuple_element<I, tuple<Types...>> element {t};
-    return element.get();
   }
 }
 
@@ -144,13 +194,15 @@ int main()
   int a {456};
   char b {'V'};
   double c {1.23456};
-  
+
 //  constexpr std::array<int, 3> arr {1,2,777};
 //  constexpr my::tuple t {123, a, b, 'A', c, arr};
 //  my::tuple t {123, a, b, 'A', c, std::string{"hello"}, 0};
 
+  namespace my = mystd::v2;
+
   Qpa qpa {};
-  my::tuple t {a, 3543, std::move(qpa), b, c, qpa};
+  mystd::tuple t {a, 3543, std::move(qpa), b, c, qpa};
 
   std::cout << std::endl;
   std::cout << my::get<0>(t) << std::endl;
